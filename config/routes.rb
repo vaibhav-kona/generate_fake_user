8e2b02fb306a62ba20dbf5e37bf257071f5b3d1f
Vaibhav <vaibhav@bluesapling.com>
2020-03-10 21:38:05 +0530

    Add users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/users', to: 'users#get'
+
+  post '/users', to: 'users#post'
+
+  put '/user/:userId', to: 'users#put'
+
+  delete '/user/:userId', to: 'user#delete'
 end
